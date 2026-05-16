27f9b461bad00491cfbf6afe7f07051a8497bd89
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2026-05-16 12:45:11 -0700

    feat(config.fish): Add script to update all pkgs once a day

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -31,3 +31,35 @@ if type -q neofetch
     neofetch
 end
 
+function update_everything_once_a_day
+    set -l stamp_dir "$HOME/.cache/fish"
+    set -l stamp_file "$stamp_dir/last-update"
+    set -l today (date +%F)
+
+    mkdir -p "$stamp_dir"
+
+    if test -f "$stamp_file"; and test (cat "$stamp_file") = "$today"
+        return
+    end
+
+    echo "$today" > "$stamp_file"
+
+    echo "Running daily updates..."
+
+    if type -q brew
+        brew update
+        brew upgrade
+        brew upgrade --cask
+        brew cleanup
+    end
+
+    if type -q cargo; and type -q cargo-install-update
+        cargo install-update --all --locked
+    end
+
+    echo "Daily updates finished."
+end
+
+if status is-interactive
+    update_everything_once_a_day
+end
